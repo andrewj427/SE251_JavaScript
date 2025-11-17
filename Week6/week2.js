e6f271cff0d5363cfd95c0386d035ef5624cad70
andrewj427 <aajohnston@email.neit.edu>
2025-11-17 11:44:21 -0500

    update assingment

--- Week6/week2.js
+++ Week6/week2.js
@@ -1,1 +1,15 @@
 // JavaScript Document
+
+let submitbtn = document.querySelector(`input[type=button]`)
+let formdiv = document.querySelector(`#form`)
+let confirmdiv = document.querySelector(`#confirmation`)
+submitbtn.addEventListener(`click`, e=> {
+    formdiv.style.display = "none"
+    confirmdiv.style.display="block"
+    let p=document.querySelector(`#info`)
+    let fname=document.querySelector("#first-name")
+    p.innerText = fname.textContent
+
+
+
+})
